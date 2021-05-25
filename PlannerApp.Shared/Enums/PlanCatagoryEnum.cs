a1aaaa37088694df31d97499fd96bf332fa433f8
Yordi van Voorthuizen <yordi.vanvoorthuizen@itvitaelearning.nl>
2021-05-25 11:30:22 +0200

    Add project files.

---
+++ PlannerApp.Shared/Enums/PlanCatagoryEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlannerApp.Shared.Enums
+{
+    public enum PlanCatagoryEnum
+    {
+       Event1,
+       Event2,
+       Event3,
+
+    }
+}
---
+++ PlannerApp.Shared/Models/PlanModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlannerApp.Shared
+{
+    public class PlanModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime Date { get; set; }
+        public string Detail { get; set; }
+
+
+
+    }
+
+}
